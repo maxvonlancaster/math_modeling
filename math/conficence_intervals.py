dc89e69aded357a53524d29b94f26bc97604145f
maxvonlancaster <windchange7@gmail.com>
2023-11-16 09:19:40 +0200

    upd

--- math/conficence_intervals.py
+++ math/conficence_intervals.py
@@ -8,48 +8,49 @@ import matplotlib.pyplot as plt
 
 ################################## PART 1
 
-
-# define sample data 
-gfg_data = [1, 1, 1, 2, 2, 2, 3, 3, 3, 3,  
-            3, 4, 4, 5, 5, 5, 6, 7, 8, 10] 
+# gfg_data = [1, 1, 1, 2, 2, 2, 3, 3, 3, 3,  
+#             3, 4, 4, 5, 5, 5, 6, 7, 8, 10] 
   
-mean = np.mean(gfg_data)
-standart_deviation = st.sem(gfg_data)
-print(f'population mean: {mean}')
-print(f'standart deviation: {standart_deviation}')
+# mean = np.mean(gfg_data)
+# standart_deviation = st.sem(gfg_data)
+# print(f'population mean: {mean}')
+# print(f'standart deviation: {standart_deviation}')
 
-# create 90% confidence interval 
-interval = st.t.interval(alpha=0.90, df=len(gfg_data), 
-              loc=mean, 
-              scale=standart_deviation) 
+# # create 90% confidence interval 
+# interval = st.t.interval(alpha=0.90, df=len(gfg_data), 
+#               loc=mean, 
+#               scale=standart_deviation) 
 
-print(interval)
+# print(interval)
 
 
 ################################### PART 2
 
 
-# mu, sigma = 5, 2 # mean and standard deviation
-# random_list = np.random.normal(mu, sigma, 1000)
-# print(random_list)
+mu, sigma = 5, 2 # mean and standard deviation
+random_list = np.random.normal(mu, sigma, 100)
+print(random_list)
 
-# interval_random = st.t.interval(alpha=0.90, df=len(random_list), 
-#               loc=np.mean(random_list), 
-#               scale=st.sem(random_list)) 
-# print(interval_random)
+interval_random = st.norm.interval(alpha=0.99,  
+              loc=np.mean(random_list), 
+              scale=st.sem(random_list)) 
+print(interval_random)
 
-# in_interval, out_interval = 0, 0
-# for i in range(10000):
-#     if(interval_random[0] < random_list[random.randint(1,999)] < interval_random[1]):
-#         in_interval += 1
-#     else:
-#         out_interval += 1
-# print(f'in the interval: {in_interval} outside: {out_interval}')
+in_interval, out_interval = 0, 0
+for i in range(1000):
+    if(interval_random[0] < random_list[random.randint(1,99)] < interval_random[1]):
+        in_interval += 1
+    else:
+        out_interval += 1
+print(f'in the interval: {in_interval} outside: {out_interval}')
 
-# mean_random = np.mean(random_list)
-# standart_deviation_random = st.sem(random_list)
+mean_random = np.mean(random_list)
+standart_deviation_random = st.sem(random_list)
 
-# count, bins, ignored = plt.hist(random_list, 100)
+print(f'population mean: {mean_random}')
+print(f'standart deviation: {standart_deviation_random}')
+
+# plt.hist(random_list, 25)
 # plt.show()
 
 
@@ -68,9 +69,10 @@ print(interval)
 
 # print(ratings)
 
-# interval_ratings = st.t.interval(alpha=0.99, df=len(ratings), 
+# interval_ratings = st.t.interval(alpha=0.99, df=len(ratings) - 1,
 #               loc=np.mean(ratings), 
 #               scale=st.sem(ratings)) 
 # print(interval_ratings)
 
-
+# plt.hist(ratings, 100)
+# plt.show()
